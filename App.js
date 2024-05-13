afc0d668001bbc380195b2a5512350396f97627e
Muhammed-shamal <muhammedshamalpv@gmail.com>
2024-05-13 10:24:30 +0530

    demo

--- App.js
+++ App.js
@@ -1,15 +1,11 @@
-// App.js
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createStackNavigator } from "@react-navigation/stack";
 import { Ionicons } from "@expo/vector-icons";
 import ProductListScreen from "./productList";
-import ProductDetailsScreen from "./productDetails";
-
-import "react-native-gesture-handler";
-import BottomNavigation from "./bottomheader";
 import NotificationScreen from "./notification";
+import ProductDetailsScreen from './productDetails'
 import CartScreen from "./cartScreen";
 import WishlistScreen from "./wishList";
 import ProfileScreen from "./profile";
@@ -17,17 +13,26 @@ import { View, Text } from "react-native";
 import LottieView from "lottie-react-native";
 import { CartProvider } from "./Context/cartContext";
 
-const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator()
 
 export default function App() {
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = React.useState(true);
 
-  useEffect(() => {
+  React.useEffect(() => {
     const timer = setTimeout(() => {
       setLoading(false);
     }, 2000); // Simulating a 2-second loading delay
     return () => clearTimeout(timer);
   }, []);
+
+
+  const HomeStack = () => (
+    <Stack.Navigator>
+      <Stack.Screen name="ProductList" component={ProductListScreen} />
+      <Stack.Screen name="ProductDetails" component={ProductDetailsScreen} />
+    </Stack.Navigator>
+  );
 
   if (loading) {
     return (
@@ -46,23 +51,64 @@ export default function App() {
   return (
     <CartProvider>
       <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen
-            name="ProductList"
-            component={ProductListScreen}
-            options={{ title: "Product List" }}
+        <Tab.Navigator
+          tabBarOptions={{
+            activeTintColor: "#e91e63",
+            inactiveTintColor: "gray",
+          }}
+        >
+          <Tab.Screen
+            name="Home"
+            component={HomeStack}
+            options={{
+              tabBarLabel: "Home",
+              tabBarIcon: ({ color, size }) => (
+                <Ionicons name="home" color={color} size={size} />
+              ),
+            }}
           />
-          <Stack.Screen
-            name="ProductDetails"
-            component={ProductDetailsScreen}
-            options={{ title: "Product Details" }}
+        
+          <Tab.Screen
+            name="Notifications"
+            component={NotificationScreen}
+            options={{
+              tabBarLabel: "Notifications",
+              tabBarIcon: ({ color, size }) => (
+                <Ionicons name="notifications" color={color} size={size} />
+              ),
+            }}
           />
-          <Stack.Screen name="Notifications" component={NotificationScreen} />
-          <Stack.Screen name="Cart" component={CartScreen} />
-          <Stack.Screen name="wishlist" component={WishlistScreen} />
-          <Stack.Screen name="profile" component={ProfileScreen} />
-        </Stack.Navigator>
-        <BottomNavigation />
+          <Tab.Screen
+            name="Cart"
+            component={CartScreen}
+            options={{
+              tabBarLabel: "Cart",
+              tabBarIcon: ({ color, size }) => (
+                <Ionicons name="cart" color={color} size={size} />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="Wishlist"
+            component={WishlistScreen}
+            options={{
+              tabBarLabel: "Wishlist",
+              tabBarIcon: ({ color, size }) => (
+                <Ionicons name="heart" color={color} size={size} />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="Profile"
+            component={ProfileScreen}
+            options={{
+              tabBarLabel: "Profile",
+              tabBarIcon: ({ color, size }) => (
+                <Ionicons name="person" color={color} size={size} />
+              ),
+            }}
+          />
+        </Tab.Navigator>
       </NavigationContainer>
     </CartProvider>
   );
